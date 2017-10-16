befd831efbed6b127e1f03066dcd0097f11d909e
Dubs <jacob.dubs@gentex.com>
2017-10-16 11:42:46 -0400

    Added some comments.  Testing commit.

---
+++ piestop/piestop.py
@@ -0,0 +1,8 @@
+#Global Values
+address = '127.0.0.1'	# The address of the spjs.
+port = '8989'		# The port that spjs is using.
+
+if __name__ == '__main__':
+	#Create the websocketclass
+
+	#Loop until button hit and send message.
