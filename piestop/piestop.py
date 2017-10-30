88c5771ad2a5fb59e6373cdee1a8e541c90e36b2
Dubs <jacob.dubs@gentex.com>
2017-10-30 16:21:57 -0400

    - Added some more logic.

--- piestop/piestop.py
+++ piestop/piestop.py
@@ -1,44 +1,58 @@
 #!/usr/bin/env python3
 
-#Global Values
-address = '127.0.0.1'	# The address of the spjs.
-port = '8989'		# The port that spjs is using.
-
 import websocket
 import threading
 import time
+import json
+
+#Global Values
+address = '127.0.0.1'	# The address of the spjs.
+port = '8989'		# The port that spjs is using.
+comport = 'COM1'	# Com port placeholder, the script will detect the one in use.
 
 def on_message(ws, message):
+	print(time.clock())
 	print(message)
+	if message.startswith('{\n\t"SerialPorts:":'):
+		print('True')
 
 def on_close(ws):
-	print('Connection closed.')
+	print('Websocket connection has closed.')
 
 def on_error(ws, error):
 	print(error)
 
 def on_open(ws):
-	print('Connection opened!')
+	print('Websocket connection opened!')
+	ws.send('list')
+	print('sent list')
 
 if __name__ == '__main__':
-	#Create the websocketclass
-	#Loop until button hit and send message.
-	ws = websocket.WebSocketApp('ws://127.0.0.1:8989/ws',
+	#Connect websocket in another thread.
+	endpoint = 'ws://{0}:{1}/ws'.format(address, port)
+	ws = websocket.WebSocketApp(endpoint,
 		on_error = on_error,
 		on_close = on_close,
 		on_message = on_message)
 
-    # Bind the on_open function.
+	# Bind the on_open function.
 	ws.on_open = on_open
 
-	# Run the ws listener thread.
+	# Run in another thread.
 	wsthread = threading.Thread(target=ws.run_forever)
 	wsthread.start()
 
-	time.sleep(3)
-	print ('Test')
-    # Do stuff while connected.
-	if ws.sock.connected:
-		print('Sending json string...')
-		ws.send('sendjson {"P":"COM3","Data":[{"D":"!"}]}')
-		print('Sent!')
+
+	# Loop until button pressed.
+	while not ws.sock.connected:
+		print('Attempting to connect...')
+		time.sleep(250)
+		
+		
+	print('Sending Feed-Hold...')
+	# ws.send('sendjson {"P":"COM3","Data":[{"D":"!"}]}')
+	ws.send('send {0} !'.format(comport))
+	print('Sent!')
+
+	wsthread.join()
+
